ea593c0b938c1d49fbcede89e538cf1e4b453bfd
Jun Steed Huang <steedhuang@ujs.edu.cn>
2020-10-28 11:41:05 -0700

    Header File for C++ Code
    This Header File runs on both LINUX and Window for real time LIDAR object recognition.

---
+++ calculation_matrix.h
@@ -0,0 +1,26 @@
+﻿/* % Copyright (C) 2020, GenieView（海沁）
+   % The algorithm is made for Joint Autonomous Vehicle Project
+   % supported by both HawkEye and RobSense
+   % The key team members are Tong Xu（徐童），Moran Huang（黄佳敏），
+   % Siqing Ma（马思清），Steed Huang（黄骏）and Hongbin Qian（钱鸿斌）。
+   % Version 0.2.3，Oct.25, 2020.
+*/
+
+#ifndef UNTITLED_CALCULATION_H
+#define UNTITLED_CALCULATION_H
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <complex>
+
+#define a 105
+#define b 140
+#define mome 1.5
+
+using namespace std;
+
+int calculation_matrix(vector<vector<int> > matrix);
+
+#endif
